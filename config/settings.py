4834ab6d3002e04e2ae67cef0a63fffdccaa615a
= <=>
2026-05-08 08:11:37 +0500

    sdfds

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50,
+    'PAGE_SIZE': 6,
 }
 
 SPECTACULAR_SETTINGS = {
